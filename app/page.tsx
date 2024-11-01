70c0e619125322801dd0955093837712e4543c59
Yash Ghugardare <yghugardare6@gmail.com>
2024-11-01 21:51:43 +0530

    add button variants and modify size

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import { Button } from "@/components/ui/button";
 
-
 export default function Home() {
   return (
-<div>
-  <Button size={"lg"}>Hello</Button>
-</div>
+    <div className="flex w-full p-5 flex-col space-y-4 justify-center ">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      {/* danger and danger outline */}
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      {/* super and super outline */}
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+
+      <Button variant="ghost">Ghost</Button>
+      {/* sidebar and sidebar outline */}
+      <Button variant="sidebar">Sidebar</Button>
+      <Button variant="sidebarOutline">Sidebar Outline</Button>
+    </div>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,29 +1,47 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl uppercase tracking-wide text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: `bg-white text-black border-slate-200 border-2 border-b-[4px] active:border-b-[2px] hover:bg-slate-100 text-slate-500`,
+        primary: `
+          bg-sky-400 text-primary-foreground border-sky-500  border-b-4 active:border-b-0 hover:bg-sky-400/90
+        `,
+        primaryOutline: `bg-white   text-sky-500  hover:bg-slate-100 `,
+        secondary: `
+          bg-green-500 text-primary-foreground border-green-600  border-b-4 active:border-b-0 hover:bg-green-500/90
+        `,
+        secondaryOutline: `bg-white   text-green-500  hover:bg-slate-100 `,
+        danger: `
+          bg-rose-500 text-primary-foreground border-rose-600  border-b-4 active:border-b-0 hover:bg-rose-500/90
+        `,
+        dangerOutline: `bg-white text-rose-500 hover:bg-slate-100 `,
+        super:`
+          bg-indigo-500 text-primary-foreground border-indigo-600  border-b-4 active:border-b-0 hover:bg-indigo-500/90
+        `,
+        superOutline: `bg-white text-indigo-500 hover:bg-slate-100 `,
+        ghost : `
+          bg-transparent text-slate-500 hover:bg-slate-100 border-0 border-transparent 
+        `,
+        sidebar : `
+          bg-transparent text-slate-500 hover:bg-slate-100 border-2 border-transparent transtion-none
+        `,
+        sidebarOutline : `
+          bg-sky-500/15 text-sky-500 hover:bg-sky-500/20 border-2 border-sky-300 transtion-none
+        `,
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-11 px-4 py-2",
+        sm: "h-9 px-3",
+        lg: "h-12  px-8",
         icon: "h-10 w-10",
+        rounded: "rounded-full",
       },
     },
     defaultVariants: {
@@ -31,26 +49,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
