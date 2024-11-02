e0b6eca86de0b196d51350d035b3a73c36c94c66
Yash Ghugardare <yghugardare6@gmail.com>
2024-11-03 01:19:25 +0530

    connect neon postgresql with drizzle orm

---
+++ db/drizzle.ts
@@ -0,0 +1,7 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import * as schema from "./schema";
+const sql = neon(process.env.DATABASE_URL!);
+// @ts-ignore
+const db = drizzle(sql, { schema });
+export default db;
