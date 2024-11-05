fb9b9fc242f36ff85466199980625e2aec443bc4
Yash Ghugardare <yghugardare6@gmail.com>
2024-11-05 15:32:31 +0530

    add query for getting units

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs";
-import { courses, userProgress } from "./schema";
+import { courses, units, userProgress,challengeProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 export const getCourses = cache(async () => {
@@ -24,4 +24,49 @@ export const getCourseById = cache(async (courseId: number) => {
     where: eq(courses.id, courseId),
     // TODO: Populate units aand lessons
   });
+});
+export const getUnits = cache(async () => {
+  const {userId} = await auth();
+  const userProgress = await getUserProgress();
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+  // TODO: Confirm whether order is needed
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: {
+                where : eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+      return {
+        ...lesson,
+        completed: allCompletedChallenges,
+      };
+    });
+    return {
+      ...unit,
+      lessons: lessonsWithCompletedStatus,
+    }
+  });
+
+  return normalizedData;
 });
